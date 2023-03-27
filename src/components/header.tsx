c778a57691536ed403ee0883226a6cac860f4f3f
grafika10 <grafika10@ucione.local>
2023-03-27 21:59:19 +0200

    navigation-components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,31 @@
 import logo from "./../assets/Apple-Logo.png";
+import hamburger from "./../assets/icon-hamburger.svg";
+
 const Header = () => {
   return (
     <header className="header">
-      <div className="header__logo">
-        <img src={logo} alt="Logo" />
-      </div>
-      <div className="header__actions">
-        <div className="header__nav">
-          <nav>
-            <a className="header__nav__item" href="">
-              Home
-            </a>
-            <a className="header__nav__item" href="">
-              About
-            </a>
-          </nav>
+      <div className="header__inner">
+        <div className="header__logo">
+          <img src={logo} alt="Logo" />
         </div>
-      </div>
-      <div>
-        <h1>MJ</h1>
+        <div className="header__actions">
+          <div className="header__nav">
+            <nav>
+              <a className="header__nav__item" href="">
+                Home
+              </a>
+              <a className="header__nav__item" href="">
+                About
+              </a>
+            </nav>
+          </div>
+          <div>
+            <h1>MJ</h1>
+          </div>
+        </div>
+        <div className="header__hamburger">
+          <img src={hamburger} alt="hamburger" />
+        </div>
       </div>
     </header>
   );
