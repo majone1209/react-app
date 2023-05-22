a26d3a24d9ac3bd5c3bddf638420b5bd0f24ff0c
grafika10 <grafika10@ucione.local>
2023-05-22 20:20:06 +0200

    Card sec change

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -9,7 +9,7 @@ const Card = ({ title, desc }: CardProps) => {
   return (
     <div className="card">
       <img
-        className="card__img"
+        className="card__img--img"
         src="https://images.unsplash.com/photo-1680849219172-71e4f632b282?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
         alt="sand"
       />
